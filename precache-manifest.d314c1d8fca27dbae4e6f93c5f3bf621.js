64833d8f1caeeaa37ed1e6a2477535c886ce6000
Lawrence <Lawrence4code@gmail.com>
2018-11-14 18:49:50 +0530

    Updates

---
+++ precache-manifest.d314c1d8fca27dbae4e6f93c5f3bf621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-ghpages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4b5a041afdd8486d619b",
+    "url": "/cra-ghpages/static/js/runtime~main.4b5a041a.js"
+  },
+  {
+    "revision": "1c80db559f06170ee189",
+    "url": "/cra-ghpages/static/js/main.1c80db55.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/cra-ghpages/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "1c80db559f06170ee189",
+    "url": "/cra-ghpages/static/css/main.3783e73d.chunk.css"
+  },
+  {
+    "revision": "825c24380f7c637badc7b8c3b8957b72",
+    "url": "/cra-ghpages/index.html"
+  }
+];
